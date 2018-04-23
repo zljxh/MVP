3a3ae63079536911af4f49a3ae8bbd83fc62c45b
zhoulang <zhoulang111111111@163.com>
2018-04-23 18:38:24 +0800

    增加 presenter模块

---
+++ app/src/main/java/jxh/zl/com/zl/presenter/MainPresenter.java
@@ -0,0 +1,8 @@
+package jxh.zl.com.zl.presenter;
+
+/**
+ * Created by HP on 2018/4/23.
+ */
+
+public class MainPresenter {
+}
---
+++ app/src/main/java/jxh/zl/com/zl/presenter/basepresenter/BasePresenter.java
@@ -0,0 +1,37 @@
+package jxh.zl.com.zl.presenter.basepresenter;
+
+/**
+ * Created by HP on 2018/4/23.
+ */
+
+public interface BasePresenter {
+    /**
+     * Method that control the lifecycle of the view. It should be called in the view's
+     * (Activity or Fragment) onResume() method.
+     */
+    void resume();
+
+    /**
+     * Method that controls the lifecycle of the view. It should be called in the view's
+     * (Activity or Fragment) onPause() method.
+     */
+    void pause();
+
+    /**
+     * Method that controls the lifecycle of the view. It should be called in the view's
+     * (Activity or Fragment) onStop() method.
+     */
+    void stop();
+
+    /**
+     * Method that control the lifecycle of the view. It should be called in the view's
+     * (Activity or Fragment) onDestroy() method.
+     */
+    void destroy();
+
+
+    /**
+     * Method that should signal the appropriate view to show the appropriate error with the provided message.
+     */
+    void onError(String message);
+}
